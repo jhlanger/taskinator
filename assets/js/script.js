225de574fed5ad691477778cc55a6a66661d484e
Jonhe <Jonhenrylangerman@gmail.com>
2021-06-29 17:54:48 -0700

    refactored javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,28 @@ var formEl = document.querySelector("#task-form");
 var taskToDoEl = document.querySelector("#tasks-to-do");
     
 
-var createTaskHandler = function () {
+var taskFormHandler = function () {
     event.preventDefault();
     //grabs text input in form
     var taskNameInput = document.querySelector("input[name='task-name']").value;
    //grabs drop down type 
     var taskTypeInput = document.querySelector("select[name = 'task-type']").value;
+    // check if input values are empty strings
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+  }
+  formEl.reset();
+    //packages the two variables above into an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+    //sned it as an argument to createTaskEl
+    createTaskEl(taskDataObj);
+};
+
+var createTaskEl = function(taskDataObj) {
     //creates a list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
@@ -18,10 +34,11 @@ var createTaskHandler = function () {
     var taskInfoEl = document.createElement("div");
     taskInfoEl.classname = "task-info";
     
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
     listItemEl.appendChild(taskInfoEl);
     
     taskToDoEl.appendChild(listItemEl);
-};
+}
 
-formEl.addEventListener ("submit", createTaskHandler);
+formEl.addEventListener ("submit", taskFormHandler);
+
