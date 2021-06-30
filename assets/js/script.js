4a37daf3cc5fb4ba55873435494f8fdab8b30757
Jonhe <Jonhenrylangerman@gmail.com>
2021-06-29 17:37:43 -0700

    add input handling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,27 @@
 
-
-var buttonEl = document.querySelector("#save-task");
+//grabs entire form, both inputs and button
+var formEl = document.querySelector("#task-form");
+//grabs unordered list from html for tasks
 var taskToDoEl = document.querySelector("#tasks-to-do");
     
 
 var createTaskHandler = function () {
-    var taskItemEl = document.createElement("li");
-    taskItemEl.textContent = "This is a new task";
-    taskItemEl.className = "task-item";
-    taskToDoEl.appendChild(taskItemEl);
+    event.preventDefault();
+    //grabs text input in form
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+   //grabs drop down type 
+    var taskTypeInput = document.querySelector("select[name = 'task-type']").value;
+    //creates a list item
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    //creates div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.classname = "task-info";
+    
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+    
+    taskToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener ("click", createTaskHandler);
+formEl.addEventListener ("submit", createTaskHandler);
